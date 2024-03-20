be1f88ab878004cc8aa3f5fef78fad1f963bfa87
James Dudeck <james.dudeck87@gmail.com>
2024-03-20 17:24:09 -0400

    Adding QFlowLayout

---
+++ ui/layouts/qflowlayout.h
@@ -0,0 +1,38 @@
+#ifndef QFLOWLAYOUT_H
+#define QFLOWLAYOUT_H
+
+#include <QLayout>
+#include <QObject>
+#include <QStyle>
+#include <QWidget>
+
+class QFlowLayout : public QLayout
+{
+public:
+    explicit QFlowLayout(QWidget *parent, int margin = -1, int hSpacing = -1, int vSpacing = -1);
+    explicit QFlowLayout(int margin = -1, int hSpacing = -1, int vSpacing = -1);
+    ~QFlowLayout();
+
+    void addItem(QLayoutItem *item) override;
+    int horizontalSpacing() const;
+    int verticalSpacing() const;
+    Qt::Orientations expandingDirections() const override;
+    bool hasHeightForWidth() const override;
+    int heightForWidth(int) const override;
+    int count() const override;
+    QLayoutItem *itemAt(int index) const override;
+    QSize minimumSize() const override;
+    void setGeometry(const QRect &rect) override;
+    QSize sizeHint() const override;
+    QLayoutItem *takeAt(int index) override;
+
+private:
+    int doLayout(const QRect &rect, bool testOnly) const;
+    int smartSpacing(QStyle::PixelMetric pm) const;
+
+    QList<QLayoutItem *> itemList;
+    int m_hSpace;
+    int m_vSpace;
+};
+
+#endif // QFLOWLAYOUT_H
