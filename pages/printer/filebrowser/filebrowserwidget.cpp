0e81d5ccbe3363a7852f39d704885acc0ccb2a61
James Dudeck <james.dudeck87@gmail.com>
2024-04-18 21:23:53 -0400

    Updated for animation change to list items

--- pages/printer/filebrowser/filebrowserwidget.cpp
+++ pages/printer/filebrowser/filebrowserwidget.cpp
@@ -11,13 +11,23 @@ void FileBrowserWidget::addFile(KlipperFile file)
 {
     FileBrowserItem *item = new FileBrowserItem(file,this);
     item->setFixedHeight(75);
+
+    setAnimationSlide(item);
     addItem(item);
 }
 
 void FileBrowserWidget::setFiles(const QList<KlipperFile> &files)
 {
-    foreach(KlipperFile file, files)
-        addFile(file);
+    for(int i = 0; i < files.count(); i++)
+    {
+        FileBrowserItem *item = new FileBrowserItem(files[i],this);
+        item->setFixedHeight(75);
+
+        setAnimationSlide(item);
+        item->setDuration(100 + (50 * i));
+
+        addItem(item);
+    }
 }
 
 FileBrowserItem *FileBrowserWidget::selectedItem()
